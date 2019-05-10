fd8b1c6628c3492eeeb834444e823b346eb60f9a
Saul Ram <saul.ramirez@disolutionsmx.com>
2019-05-09 22:19:02 -0500

    firebase configurated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyD2mbiYmOsskaU3InWFIHSXWl0NbIDK6-w",
+    authDomain: "schoolapp-1aca7.firebaseapp.com",
+    databaseURL: "https://schoolapp-1aca7.firebaseio.com",
+    projectId: "schoolapp-1aca7",
+    storageBucket: "schoolapp-1aca7.appspot.com",
+    messagingSenderId: "393605770189",
+    appId: "1:393605770189:web:f65c99f580571b8a"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyD2mbiYmOsskaU3InWFIHSXWl0NbIDK6-w",
+    authDomain: "schoolapp-1aca7.firebaseapp.com",
+    databaseURL: "https://schoolapp-1aca7.firebaseio.com",
+    projectId: "schoolapp-1aca7",
+    storageBucket: "schoolapp-1aca7.appspot.com",
+    messagingSenderId: "393605770189",
+    appId: "1:393605770189:web:f65c99f580571b8a"
+  }
 };
 
 /*
