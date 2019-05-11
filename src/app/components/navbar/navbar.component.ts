11e37b7ff58f701ea366488b09b8ed5a0a6cfd60
Saul Ram <saul.ramirez@disolutionsmx.com>
2019-05-10 22:24:15 -0500

    Resuelto navbar con login

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -17,21 +17,42 @@ export class NavbarComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.currentuser = this.authService.doSomething();
-    if (this.currentuser != null) {
-      this.isLogin=true;
-      
-      console.log(this.currentuser +""+ this.isLogin);
-      
-    }else{
-      this.isLogin=false;
+    /** Cuando iniciamos sesion, se setea un item en Session Storage (volatil al cerrar el browser), si ese elemento existe, ya no hace comprobacion a firebase
+    si no lo encuentra, procede a verificar con firebase si estamos logueados
+    */
+    if (sessionStorage.getItem("auth") == "ok") {
+
+      this.isLogin = true;
+
+    } else {
+      //se conecta al metodo de "check login " de nuestro Auth Service, una vez terminado .then va a recibir un usuario de firebase llamado auth el cual leemos
+      //si es nulo, quiere decir que no hay usuario loggueado, si no es nulo, nos devuelve un objeto de firebase
+      this.authService.doSomething().then((auth) => {
+
+        if (auth) {
+          sessionStorage.setItem("auth", "ok");
+        }
+        if (auth == null) {
+          this.isLogin = false
+        } else {
+          this.isLogin = true;
+          this.nombreuser = auth.displayName;
+          this.email = auth.email;
+        }
+
+      })
+
     }
+
 
   }
   onClickLogOut() {
-    this.isLogin=false;
-    this.authService.logout();
-    this.router.navigate(["/login"]);
+    this.isLogin = null;
+    this.authService.logout().then(() => {
+      sessionStorage.removeItem("auth");
+      this.router.navigate(['']);
+    });
+
 
   }
 
